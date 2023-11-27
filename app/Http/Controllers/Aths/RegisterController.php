6f94841cd9487fe52e88028521ae48630dc90a49
84arshia84 <arshiia.a84@gmail.com>
2023-11-27 14:07:12 +0330

    add RegisterController

--- app/Http/Controllers/Aths/RegisterController.php
+++ app/Http/Controllers/Aths/RegisterController.php
@@ -13,17 +13,15 @@ class RegisterController extends Controller
 {
     public function sendVerificationCode(Request $request)
     {
-
         $rand = rand(1000, 9999);
-        $timValid = Carbon::now()->addMinutes(5);
+        $timeValid = Carbon::now()->addMinutes(5);
         $send_sms = TempCode::updateOrCreate([
             'phone_number' => $request->phone_number,
             'verification_cod' => $rand,
-            'exp_time' => $timValid,
+            'exp_time' => $timeValid,
         ]);
-//        $this->send_sms($request, $rand);
-
-        return response()->json(['phone_number']);
+        $this->send_sms($request, $rand);
+        return response()->json(['phone_number']);    // Change text
     }
 
     protected function send_sms(Request $request, $rand)
@@ -37,25 +35,53 @@ class RegisterController extends Controller
             "qk9hlr3czoqct8t",
             "+983000505",      // originator
             $request->phone_number,  // recipient
-            $patternValues,  // pattern values
+            $patternValues  // pattern values
         );
 
 
     }
 
-    public function register(Request $request)
+    public function store(Request $request)
     {
-
         $request->validate([
             'name' => 'required|string|max:255',
             'family' => 'required|string|max:255',
-            'fatherName' => 'required|string|max:255',
+            'father_name' => 'required|string|max:255',
             'phone_number' => 'required|string|max:11|unique:users',
             'national_id' => 'required',
-            'password' => 'required|string|min:6',
+            'password' => '',
         ]);
-//dd($request);
-        $user = User::update($request->all());
+        $user = User::create($request->all());
+        return response()->json(['user' => $user
+        ]);
+
+    }
+
+    public function Add_password(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->update($request->all());
+        $user->save();
+        return response()->json([
+            $user
+        ]);
+    }
+
+    public function register(Request $request, $id)
+    {
+//        $request->validate([
+//            'name' => 'required|string|max:255',
+//            'family' => 'required|string|max:255',
+//            'father_name' => 'required|string|max:255',
+//            'phone_number' => 'required|string|max:11|unique:users',
+//            'national_id' => 'required',
+//            'password' => 'required|string|min:6',
+//        ]);
+//        $user = User::update($request->all());
+//        $user = User::update($request->all());
+        $user = User::find($id);
+        $user->update($request->all());
+        $user->save();
         $token = $user->createToken('api_token')->plainTextToken;
         return response()->json([
             'user' => $user,
@@ -63,16 +89,15 @@ class RegisterController extends Controller
         ]);
     }
 
-    public function match(Request $request)
+    public function check(Request $request)
     {
         $Code_confirmation_time = Carbon::now();
-
 
 
         $status = TempCode::where('phone_number', $request->phone_number)->where('verification_cod', $request->verification_cod)->first();
         {
             if ($status != null) {
-               return response()->json($Code_confirmation_time ,'phone_number');
+                return response()->json( 'کد تایید شد');
             } else {
                 return response()->json('کد اشتباه است');
             }
