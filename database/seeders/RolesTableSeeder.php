8b833cfee325a19369e4c9d99f7a2079f8da6638
84arshia84 <140607046+84arshia84@users.noreply.github.com>
2024-01-16 11:40:06 +0330

    show_all_transaction

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -14,10 +14,10 @@ class RolesTableSeeder extends Seeder
     public function run(): void
     {
         // ایجاد رول‌ها
-        $admin = Role::create(['name' => 'admin']);
-        $user = Role::create(['name' => 'user']);
+//        $admin = Role::create(['name' => 'admin']);
+//        $user = Role::create(['name' => 'user']);
         // اضافه کردن رول‌های دیگر
-$user->givePermissionTo('user.add', 'user.update', 'user.image', 'user.show.info', 'user.change.password', 'user.Email.update', 'user.profile.img',    'add.Bank.account ',    'loan.add', ' loan.delete', ' Returning.the.deleted.loan', ' Loan.details',' all.loans.for.user' , 'installment.find', 'installment.all', 'Installments.paid', 'installment.Deferred', 'Add.money.to.user.account', 'WalletBalance', 'showUserTransactions', 'showUserTransactionId', 'paySubscription', 'payment.callback', 'showUserPaidInstallments', 'transaction.show', 'Bank.receipt.photo', 'update.status.Bank.receipt.photo');
-$admin->givePermissionTo(Permission::all());
+//$user->givePermissionTo('user.add', 'user.update', 'user.image', 'user.show.info', 'user.change.password', 'user.Email.update', 'user.profile.img',    'add.Bank.account ',    'loan.add', ' loan.delete', ' Returning.the.deleted.loan', ' Loan.details',' all.loans.for.user' , 'installment.find', 'installment.all', 'Installments.paid', 'installment.Deferred', 'Add.money.to.user.account', 'WalletBalance', 'showUserTransactions', 'showUserTransactionId', 'paySubscription', 'payment.callback', 'showUserPaidInstallments', 'transaction.show', 'Bank.receipt.photo', 'update.status.Bank.receipt.photo');
+//$admin->givePermissionTo(Permission::all());
     }
 }
